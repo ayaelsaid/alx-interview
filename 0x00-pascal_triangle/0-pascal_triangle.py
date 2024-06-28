97346d1b34f42274549a7b4375de96a328da22c0
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 18:55:12 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''function that prints pascal_triangle'''
 
+
 def pascal_triangle(n):
     """
     Print Pascal's triangle up to n rows.
@@ -12,27 +13,27 @@ def pascal_triangle(n):
     None
     """
     pascal_triangle = list()  # Initialize Pascal's triangle as an empty list
-    
+
     if n <= 0:
         return pascal_triangle
 
     # Add first row [1]
-    pascal_triangle.append([1])
+    triangle.append([1])
 
     # Add second row [1, 1]
     if n > 1:
-        pascal_triangle.append([1, 1])
+        triangle.append([1, 1])
 
     # Add subsequent rows
     for x in range(3, n + 1):
-        new_row = [0] * x
-        new_row[0] = 1  # Set first element to 1
-        new_row[-1] = 1  # Set last element to 1
+        row = [0] * x
+        row[0] = 1  # Set first element to 1
+        row[-1] = 1  # Set last element to 1
 
         # Calculate middle elements
         for y in range(1, x - 1):
-            new_row[y] = pascal_triangle[x - 2][y - 1] + pascal_triangle[x - 2][y]
+            row[y] = triangle[x - 2][y - 1] + triangle[x - 2][y]
 
-        pascal_triangle.append(new_row)
+        triangle.append(row)
 
-    return pascal_triangle
+    return triangle
