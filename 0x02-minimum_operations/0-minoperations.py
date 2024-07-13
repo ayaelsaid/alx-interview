2b77076d7b1f7163329bdcb41a1edf5b3605c244
ayaelsaid <ayae7675@gmail.com>
2024-07-13 23:21:06 +0000

    first

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""method that calculates the fewest number of operations"""
+
+def minOperations(n):
+    """
+    method that calculates the fewest number of operations
+    """
+    if n <= 0:
+        return 0
+    factor = 2
+    op = 0
+
+    while n > 1:
+        while n % factor == 0:
+            op += factor
+            n //= factor
+        
+        factor += 1
+    return op
