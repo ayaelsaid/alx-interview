58d37038454af74c0350b7ea388a98244f8ef8c5
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 01:10:46 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" Making changes """
 
 
 def makeChange(coins, total):
+    """ find the min coins to total
+
+    Args:
+        coins([List]): list of coins
+        total([int]): total amount needed
+    """
     if total <= 0:
         return 0
     
@@ -13,5 +20,4 @@ def makeChange(coins, total):
             if amount >= coin:
                 res = amount - coin
                 min_coins[amount] = min(min_coins[res] + 1, min_coins[amount])
-
     return min_coins[total] if min_coins[total] != float('inf') else -1
