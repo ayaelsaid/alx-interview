b26728d6a64de9fe125b1565f9ef1c96cd43ee37
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-18 05:08:36 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,29 +2,6 @@
 """Defines playing a game with primes."""
 
 
-def find_prime(n):
-    """
-    Find the prime numbers up to n.
-    Args:
-        n (int): The upper limit to find primes (inclusive).
-    Returns:
-        list: A list of prime numbers from 2 to n (inclusive).
-    """
-    if n < 2:
-        return [] 
-    primes = [True] * n
-    primes[0] = False
-
-    p = 2
-    while p * p <= n:
-        if primes[p - 1]:
-            for i in range(p * p, n + 1, p):
-                primes[i - 1] = False
-        p += 1
-    all_primes = [i for i in range(2, n + 1) if primes[i - 1]]
-    return all_primes
-
-
 def isWinner(x, nums):
     """
     Determine the winner of the prime game between Maria and Ben.
@@ -38,21 +15,40 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    if x < 10000:
-        for i in range(x):
-            n = nums[i]
-            if n < 2:
-                ben += 1
-                continue
-            primes = find_prime(n)
-            used_primes = len(primes)
-            if used_primes % 2 == 0:
-                ben += 1
-            else:
-                maria += 1
+    for i in range(x):
+        n = nums[i]
+        if n < 2:
+            ben += 1
+            continue
+
+        primes = find_prime(n)
+        used_primes = len(primes)
+
+        if used_primes % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
     if ben > maria:
         return "Ben"
     elif maria > ben:
         return "Maria"
     else:
         return None
+
+
+def find_prime(n):
+    """Find the prime numbers up to n."""
+    if n < 2:
+        return [] 
+    primes = [True] * n
+    primes[0] = False
+
+    p = 2
+    while p * p <= n:
+        if primes[p - 1]:
+            for i in range(p * p, n + 1, p):
+                primes[i - 1] = False
+        p += 1
+    all_primes = [i for i in range(2, n + 1) if primes[i - 1]]
+    return all_primes
